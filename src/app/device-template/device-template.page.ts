0ffc7358edb6b6306fa0c60e29b1161f9f3f2e21
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2021-02-13 01:56:21 +0100

    Delete Device Template

--- src/app/device-template/device-template.page.ts
+++ src/app/device-template/device-template.page.ts
@@ -2,6 +2,7 @@ import { DeviceTemplate } from './../models/deviceTemplate.model';
 import { DeviceTemplateService } from './../services/deviceTemplate.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Data } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-device-template',
@@ -11,7 +12,9 @@ import { ActivatedRoute, Router, Data } from '@angular/router';
 export class DeviceTemplatePage implements OnInit {
   public listDeviceTemplates: DeviceTemplate[] = [];
   constructor(
-            private deviceTemplateService : DeviceTemplateService) {}
+            private deviceTemplateService : DeviceTemplateService,
+            public alertController :AlertController
+            ) {}
 
   ngOnInit():void {
     this.deviceTemplateService.getAllDeviceTemplate()
@@ -20,6 +23,36 @@ export class DeviceTemplatePage implements OnInit {
       },(err) => {
           console.log(err);
       });
+  }
+
+ async deleteDeviceTemplate(id : number, name: string){
+     console.log(id);
+     const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Remove Device Template',
+      message: `Are you sure you want remove ${name}?`,
+      buttons: [  {
+        text: 'Cancel',
+        handler: () => {
+          console.log('Disagree clicked');
+        }
+      },
+      {
+        text: 'Agree',
+        handler: () => {
+          console.log('Agree clicked');
+          this.deviceTemplateService.deleteDeviceTemplate(id)
+          .subscribe( (res : any) => {
+            window.location.reload();
+          },(err) => {
+              console.log(err);
+          });
+        }
+      }]
+    });
+
+    await alert.present();
+
   }
  
 
