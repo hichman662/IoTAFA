0cd5b3fb69b6f055287865e62e63486be31857b8
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2021-03-21 15:21:06 +0100

    Add disability

--- src/app/patient-profile/add-disability/add-disability.component.ts
+++ src/app/patient-profile/add-disability/add-disability.component.ts
@@ -1,4 +1,10 @@
+import { PatientProfileService } from './../../services/patientProfile.service';
 import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-add-disability',
@@ -7,8 +13,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDisabilityComponent implements OnInit {
 
-  constructor() { }
+  disabilityForm: FormGroup;
+  idDisability: number;
+  name = '';
+  constructor(
+    private router: Router,
+    private patientProfileService: PatientProfileService,
+    public alertController: AlertController,
+    private route: ActivatedRoute,
+    private storage: Storage
+  ) {
+    this.disabilityForm = new FormGroup({
+      Patient_oid: new FormControl(),
+      Name: new FormControl('', [
+        Validators.required
+      ]),
+      Type: new FormControl('', [
+        Validators.required
+      ]),
+      Severity: new FormControl('', [
+        Validators.required
+      ]),
+      Origin_oid: new FormControl(0),
+      AccessMode_oid : new FormControl([0])
+    });
+  }
 
-  ngOnInit() {}
+  ngOnInit(){
+    this.storage.get('idPatientProfile').then((val) => {
+      console.log('I´m carrying id Patient Profile', val);
+      this.disabilityForm.get('Patient_oid').setValue(val);
+      console.log(this.disabilityForm.value.Patient_oid);
+    });
+  }
+
+  async saveAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'SUCCESS!',
+      message: `Disability ${this.name} has been added successfully`,
+      buttons: [
+      {
+        text: 'Ok',
+        handler: () => {
+          /* this.router.navigateByUrl('tabs/tab1/device-template/add-device-template'); */
+        }
+      }]
+    });
+
+    await alert.present();
+}
+onSubmit() {
+  this.ngOnInit();
+  this.patientProfileService.createDisability(this.disabilityForm.value)
+  .subscribe( (res: any) => {
+    console.log(res);
+    console.log('Disability added');
+    this.name = res['Name'];
+    this.saveAlert();
+  }, ( err ) => {
+
+  });
+
+  this.disabilityForm.reset();
+}
 
 }
--- src/app/patient-profile/add-patient-profile/add-patient-profile.module.ts
+++ src/app/patient-profile/add-patient-profile/add-patient-profile.module.ts
@@ -1,3 +1,4 @@
+import { AddDisabilityComponent } from './../add-disability/add-disability.component';
 import { RouterModule } from '@angular/router';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { NgModule } from '@angular/core';
@@ -9,6 +10,8 @@ import { IonicModule } from '@ionic/angular';
 import { AddPatientProfilePageRoutingModule } from './add-patient-profile-routing.module';
 
 import { AddPatientProfilePage } from './add-patient-profile.page';
+import { AddConditionComponent } from '../add-condition/add-condition.component';
+import { AddAccessModeComponent } from '../add-access-mode/add-access-mode.component';
 
 @NgModule({
   imports: [
@@ -20,6 +23,6 @@ import { AddPatientProfilePage } from './add-patient-profile.page';
     MatExpansionModule,
      RouterModule
   ],
-  declarations: [AddPatientProfilePage]
+  declarations: [AddPatientProfilePage, AddDisabilityComponent, AddConditionComponent, AddAccessModeComponent]
 })
 export class AddPatientProfilePageModule {}
