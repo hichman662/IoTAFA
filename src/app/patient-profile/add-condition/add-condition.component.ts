55080e81d022455a00d032a4e3e8bfd0a24986c4
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2021-03-21 22:25:23 +0100

    Add condition

--- src/app/patient-profile/add-condition/add-condition.component.ts
+++ src/app/patient-profile/add-condition/add-condition.component.ts
@@ -1,4 +1,10 @@
+import { PatientProfileService } from './../../services/patientProfile.service';
 import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-add-condition',
@@ -7,8 +13,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddConditionComponent implements OnInit {
 
-  constructor() { }
+  
+  conditionForm: FormGroup;
+  idDisability: number;
+  name = '';
+  constructor(
+    private router: Router,
+    private patientProfileService: PatientProfileService,
+    public alertController: AlertController,
+    private route: ActivatedRoute,
+    private storage: Storage
+  ) {
+    this.conditionForm = new FormGroup({
+      PatientProfile_oid: new FormControl(),
+      Disease: new FormControl('', [
+        Validators.required
+      ]),
+      Description: new FormControl('', [
+        Validators.required
+      ]),
+      DateInitial: new FormControl(new Date().toISOString(), [
+        Validators.required
+      ]),
+      DateEnd: new FormControl(new Date().toISOString()),
+      ClinicalStatus: new FormControl('', [
+        Validators.required
+      ])
+    });
+  }
 
-  ngOnInit() {}
+  ngOnInit(){
+    this.storage.get('idPatientProfile').then((val) => {
+      console.log('I´m carrying id Patient Profile', val);
+      this.conditionForm.get('PatientProfile_oid').setValue(val);
+      console.log(this.conditionForm.value.PatientProfile_oid);
+    });
+  }
+
+  async saveAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'SUCCESS!',
+      message: `Condition ${this.name} has been added successfully`,
+      buttons: [
+      {
+        text: 'Ok',
+        handler: () => {
+          /* this.router.navigateByUrl('tabs/tab1/device-template/add-device-template'); */
+        }
+      }]
+    });
+
+    await alert.present();
+}
+onSubmit() {
+  this.ngOnInit();
+  this.patientProfileService.createCondition(this.conditionForm.value)
+  .subscribe( (res: any) => {
+    console.log(res);
+    console.log('Condition added');
+    this.name = res['Description'];
+    this.saveAlert();
+  }, ( err ) => {
+
+  });
+
+  this.conditionForm.reset();
+}
+
 
 }
