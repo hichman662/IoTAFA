5a86e240e331cd30b0017fa191ee6a32589051c0
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2021-04-14 01:47:34 +0200

    Add care plan Template Advance

--- src/app/care-plan-template/add-care-plan-template/add-care-plan-template.page.ts
+++ src/app/care-plan-template/add-care-plan-template/add-care-plan-template.page.ts
@@ -1,3 +1,5 @@
+import { PatientProfileService } from './../../services/patientProfile.service';
+import { Condition } from './../../models/condition.model';
 import { CarePlanTemplateService } from './../../services/carePlanTemplate.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
@@ -16,36 +18,72 @@ export class AddCarePlanTemplatePage implements OnInit {
 
   // tslint:disable-next-line: ban-types
   segmentModel = 'carePlan';
+  segmentModel2 = 'profile';
   carePlanOk = false;
   name = '';
   idCarePlan: number;
   nameCarePlan: '';
   carePlanForm: FormGroup;
   carePlanTemplate: CarePlanTemplate ;
-
+  conditionForm: FormGroup;
+  allConditions: Condition[] = [];
+  
   constructor(
     private carePlanTemplateService: CarePlanTemplateService,
+    private patientProfileService: PatientProfileService,
+
     public alertController: AlertController,
     private router: Router,
     private storage: Storage
   ) {
 
     this.carePlanTemplate = null;
+    this.conditionForm = null;
 
     this.carePlanForm = new FormGroup({
-      name: new FormControl('', [
+      Title: new FormControl('', [
         Validators.required
       ]),
-      isEdge: new FormControl(true),
-      type: new FormControl('', [
+      Name: new FormControl('', [
         Validators.required
       ]),
+      Description: new FormControl('', [
+        Validators.required
+      ]),
+      DurationDays: new FormControl(Number, [
+        Validators.required
+      ]),
+      Status: new FormControl(Number, [
+        Validators.required
+      ]),
+      Intent: new FormControl(Number, [
+        Validators.required
+      ]),
+      Modified: new FormControl(new Date().toISOString(), [
+        Validators.required
+      ]),
+    });
+
+    this.conditionForm = new FormGroup({
+      p_careplantemplate_oid: new FormControl(Number, [
+        Validators.required
+      ]),
+      p_addressconditions_oids: new FormControl([Number], [
+        Validators.required
+      ])
     });
    }
 
    ionViewWillEnter(){}
 
-   ngOnInit() {}
+   ngOnInit() {
+
+    this.patientProfileService.getAllCondition()
+    .subscribe( (res: any) => {
+      this.allConditions = res;
+        }, ( err ) => {
+    });
+   }
 
    onSubmit(){
     this.carePlanTemplate = this.carePlanForm.value;
@@ -60,7 +98,7 @@ export class AddCarePlanTemplatePage implements OnInit {
       this.nameCarePlan = res['Name'];
       this.carePlanOk = true;
       this.storage.set('idCarePlan', this.idCarePlan);
-
+      this.conditionForm.get('p_careplantemplate_oid').setValue(this.idCarePlan);
       this.presentAlert();
     }, ( err ) => {
 
