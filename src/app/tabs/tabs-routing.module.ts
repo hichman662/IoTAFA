f27e9222f954837d8adf79023b993d8bc41025d0
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2021-04-16 01:06:59 +0200

    problem

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,12 +4,6 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-<<<<<<< HEAD
-    path: 'tabs',
-    component: TabsPage,
-    children: [
-      {
-=======
     path: '',
     component: TabsPage,
     children: [
@@ -18,7 +12,6 @@ const routes: Routes = [
         loadChildren: () => import('../tab0/tab0.module').then(m => m.Tab0PageModule)
       },
       {
->>>>>>> 5831dd3e7036d35a3c7bca27167a93f83e60562e
         path: 'tab1',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
@@ -31,25 +24,12 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
-<<<<<<< HEAD
-        path: '',
-        redirectTo: '/tabs/tab1',
-=======
         path: '**',
         redirectTo: '/tabs/tab0',
->>>>>>> 5831dd3e7036d35a3c7bca27167a93f83e60562e
         pathMatch: 'full'
       }
     ]
   },
-<<<<<<< HEAD
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
-=======
->>>>>>> 5831dd3e7036d35a3c7bca27167a93f83e60562e
 ];
 
 @NgModule({
