75477dc5da78cf4ac04882f282ac6fbf2d8d89a4
Seyed Shahabadin Nasabeh <hichman662@gmail.com>
2021-09-01 02:58:54 +0200

    metricas initial page

--- src/app/tab0/tab0.page.ts
+++ src/app/tab0/tab0.page.ts
@@ -1,3 +1,6 @@
+import { CarePlanTemplateService } from './../services/carePlanTemplate.service';
+import { PatientProfileService } from './../services/patientProfile.service';
+import { DeviceTemplateService } from './../services/deviceTemplate.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab0.page.scss'],
 })
 export class Tab0Page implements OnInit {
-
-  constructor() { }
+totalDeviceTemplate: number = 0;
+totalPatientprofile: number = 0;
+totalCarePlanTemplate: number = 0;
+  constructor( private deviceTemplateService: DeviceTemplateService,
+   private patientProfileService: PatientProfileService,
+   private carePlanTemplateService: CarePlanTemplateService) { }
 
   ngOnInit() {
   }
 
+  ionViewWillEnter() {
+
+    // Device template
+    this.deviceTemplateService.getAllDeviceTemplate()
+    // tslint:disable-next-line: deprecation
+    .subscribe( (res: any) => {
+        this.totalDeviceTemplate = res.length;
+    }, ( err) => {
+        console.log(err);
+    });
+// patient profile 
+    this.patientProfileService.getAllPatientProfile()
+    .subscribe( (res: any) => {
+        this.totalPatientprofile = res.length;
+    }, ( err) => {
+        console.log(err);
+    });
+    
+// Care Plan Template
+    this.carePlanTemplateService.getAllCarePlantemplate()
+    // tslint:disable-next-line: deprecation
+    .subscribe( (res: any) => {
+        this.totalCarePlanTemplate = res.length;
+    }, ( err) => {
+        console.log(err);
+    });
+}
 }
